621f0f8ba2376a7a197bc41db38e5f5546bec272
co.alphacodes@gmail.com <co.alphacodes@gmail.com>
2020-05-20 18:27:21 +0430

    simple view support

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-export default {}
+export function create ({ views = {}, prefix = '', routerOptions = {} } = {}) {
+  const { routes = [] } = routerOptions
+  prefix = prefix.replace(/(^\/+)|(\/+$)/g, '').replace(/(.+)/g, '$1/')
+  routerOptions.routes = routes.concat([
+    {
+      path: `/${prefix}article/:id`,
+      component: views.Article || () => import(/* webpackChunkName: "plugin-cms-article" */ './views/Article'),
+    },
+    {
+      path: `/${prefix}category/:id`,
+      component: views.Category || () => import(/* webpackChunkName: "plugin-cms-category" */ './views/Category'),
+    },
+    {
+      path: `/${prefix}tag/:id`,
+      component: views.Tag || () => import(/* webpackChunkName: "plugin-cms-tag" */ './views/Tag'),
+    },
+  ])
+}
